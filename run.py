da3312c6b2bef4c9689b1cfcc30d8e512e8ed9fe
Grzegorz Ciołek <ciolek.greg@gmail.com>
2020-08-30 22:02:56 +0100

    Fixed folder creation bug.

--- run.py
+++ run.py
@@ -1,9 +1,9 @@
 import os
 import src.main
 
-if os.path.isdir(os.path.join(os.path.dirname(__file__), "bin")) == False:
+if os.path.isdir(os.path.join(os.path.dirname(__file__), "../bin")) == False:
     os.makedirs("bin")
-if os.path.isdir(os.path.join(os.path.dirname(__file__), "dst")) == False:
+if os.path.isdir(os.path.join(os.path.dirname(__file__), "../dst")) == False:
     os.makedirs("dst")
 
 src.main.run()
