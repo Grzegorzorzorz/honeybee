c055b9e0184cf14d67961dbaf17b53e1e72bd79c
Grzegorz Ciołek <ciolek.greg@gmail.com>
2020-09-02 04:50:06 +0100

    Renamed run.py to honeybee.py. Added verbosity.

--- honeybee.py
+++ honeybee.py
@@ -1,9 +1,13 @@
 import os
 import src.main
 
+# Check if the bin and dst directories are present, and if not, generate them.
 if os.path.isdir(os.path.join(os.path.dirname(__file__), "bin")) == False:
+    print("'bin' directory not found. Generating...")
     os.makedirs(os.path.join(os.path.dirname(__file__), "bin"))
 if os.path.isdir(os.path.join(os.path.dirname(__file__), "dst")) == False:
+    print("'dst' directory not found. Generating...")
     os.makedirs(os.path.join(os.path.dirname(__file__), "dst"))
 
+# Start the program.
 src.main.run()
